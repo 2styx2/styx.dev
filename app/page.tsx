dbf1cf3158b76262497dd271c7da1ad27be61c0f
2styx2 <bomber2244@gmail.com>
2025-09-23 00:09:03 -0400

    added scramble text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import { BlogPosts } from 'app/components/posts'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { Redis } from '@upstash/redis';
-import { formatDate, getBlogPosts } from 'app/blog/utils'
+import { BlogPosts } from "app/components/posts";
+import { ScrambleText } from "./components/scramble";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { Redis } from "@upstash/redis";
+import { formatDate, getBlogPosts } from "app/blog/utils";
 
 const redis = Redis.fromEnv();
 
@@ -42,47 +43,42 @@ function ProjectSection({
   );
 }
 
-
 export default async function Page() {
   let allPosts = getBlogPosts();
-  const views = (
-    await redis.mget<number[]>(
-      ...allPosts.map((post) => ['pageviews', 'posts', post.slug].join(":"))
-    )
-  )
+  const views = await redis.mget<number[]>(
+    ...allPosts.map((post) => ["pageviews", "posts", post.slug].join(":")),
+  );
 
   const projectItems = [
     {
       name: "xd",
       link: "https://github.com/2styx2/xd",
       position: "creator and maintainer",
-      description:
-        "simple directory navigation tool",
+      description: "simple directory navigation tool",
     },
     {
       name: "hugin",
       link: "https://github.com/2styx2/hugin",
       position: "creator and maintainer",
-      description:
-        "personal os with with the ia32 architecture",
+      description: "personal os with with the ia32 architecture",
     },
-  ]
+  ];
 
   return (
     <section>
       <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
-        wesley matthews
+        <ScrambleText text="wesley matthews" />
       </h1>
       <div className="mb-6 flex flex-col gap-2">
-      <div className="flex items-center gap-3 text-neutral-700 dark:text-neutral-300" >
-        <FontAwesomeIcon icon={faLocationDot} /> 
-        <p className="text-zinc-300">belmont, nc</p>
-      </div>
+        <div className="flex items-center gap-3 text-neutral-700 dark:text-neutral-300">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <p className="text-zinc-300">belmont, nc</p>
+        </div>
       </div>
       <p className="mb-4 text-zinc-300">
-        {`i'm a 21 y/o cs student at nc state. 
+        {`i'm a 21 y/o cs student at nc state.
         i love building dev tools and solving problems. i enjoy how languages are designed
-        and i live in the terminal. if i'm not at my computer, 
+        and i live in the terminal. if i'm not at my computer,
         i'm probably playing with my dogs, watching tv shows or planning my next project.`}
       </p>
 
@@ -90,5 +86,5 @@ export default async function Page() {
         <ProjectSection title="projects" items={projectItems} />
       </div>
     </section>
-  )
+  );
 }
